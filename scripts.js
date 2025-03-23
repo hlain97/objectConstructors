7d593adbf9dc8e2d368cb6c4a0d67fc0d9983231
hlain97 <halimnakad97@gmail.com>
2025-03-23 00:02:05 -0300

    Built a function to create new books

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let button = document.getElementById("btn");
+
+button.addEventListener("click", (e) => console.table(bookshelf))
 function createBook(title, author, pages, read){
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
@@ -10,9 +13,14 @@ function createBook(title, author, pages, read){
     this.info = () => `The Title is ${newBook.title}, written by ${newBook.author}, the number of pages is ${newBook.pages}, and the status is ${newBook.read}`;
 }
 
-const newBook = new createBook("Learning to Code", "Halim", "420", "Read");
+let bookshelf = [];
 
-console.log(newBook.info());
+let addBook = function(title, author, pages, read){
+    let newBook = new createBook(title, author, pages, read);
+    bookshelf.push(newBook);
+};
 
-let tester = Object.getPrototypeOf(newBook)
-console.log(tester)
+addBook('Learnin Programming', 'Halim', '420', 'Read');
+addBook('Doing Dr0gs', 'Halim', '420', 'Read');
+addBook('ROFLCOPTER', 'Halim', '420', 'Read');
+
